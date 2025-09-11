464eb16f0b421c6230b8cbdd675b2d434b9b2764
Sina Khalili <paradox@Mac.lan>
2025-09-11 18:58:28 +0330

    Loding Screen

---
+++ script.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", () => {
+
+    const loader = document.getElementById("loader");
+    const body = document.body;
+  
+    const startTime = performance.timing.navigationStart;
+  
+    const loadTime = Date.now() - startTime;
+  
+    const minDuration = 3000;
+  
+    const delay = loadTime < minDuration ? (minDuration - loadTime) : 0;
+  
+    setTimeout(() => {
+      loader.classList.add("hidden");
+      body.classList.remove("loading");
+    }, delay);
+});
