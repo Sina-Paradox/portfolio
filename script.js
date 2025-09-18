96d0077cf765263c304bface007c0f50096e5187
Sina Khalili <paradox@Mac.lan>
2025-09-18 23:40:48 +0330

    google login bug fix

--- script.js
+++ script.js
@@ -25,36 +25,11 @@ window.scrollTo(0, 0);
 
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDeZRotr7SVHg-aqRqR9pNFpOJTMFwKQXo",
-  authDomain: "portfolio-7d11f.firebaseapp.com",
-  projectId: "portfolio-7d11f",
-  storageBucket: "portfolio-7d11f.firebasestorage.app",
-  messagingSenderId: "256853786845",
-  appId: "1:256853786845:web:80c314acbb77b68ab2db34",
-  measurementId: "G-RQ0ZP0GY0R"
-};
-
-const app = firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
 const authContainer = document.getElementById('authContainer');
 const googleAuthButton = document.getElementById('googleAuthBtn');
 const authButtonContent = document.getElementById('authButtonContent');
 const authDropdown = document.getElementById('authDropdown');
 const signOutButton = document.getElementById('signOutButton');
-
-function signInWithGoogle() {
-  const provider = new firebase.auth.GoogleAuthProvider();
-
-  auth.signInWithPopup(provider)
-    .then((result) => {
-      console.log('Success! Signed in user:', result.user);
-
-    }).catch((error) => {
-      console.error("Error during sign-in:", error);
-      alert("Login failed: " + error.message);
-    });
-}
 
 function updateUI(user) {
   if (user) {
@@ -73,16 +48,6 @@ function toggleDropdown() {
 
 auth.onAuthStateChanged(function(user) {
   updateUI(user);
-});
-
-googleAuthButton.addEventListener('click', function() {
-  const user = auth.currentUser;
-
-  if (user) {
-    toggleDropdown();
-  } else {
-    signInWithGoogle();
-  }
 });
 
 signOutButton.addEventListener('click', function() {
